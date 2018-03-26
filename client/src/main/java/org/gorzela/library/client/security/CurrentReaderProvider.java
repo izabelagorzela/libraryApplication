e737009fa4c1e6432fd83ba2061ea53e3acb198b
Iza <jza.q@wp.pl>
2018-03-26 20:24:30 +0200

    Work in progress

--- client/src/main/java/org/gorzela/library/client/security/CurrentReaderProvider.java
+++ client/src/main/java/org/gorzela/library/client/security/CurrentReaderProvider.java
@@ -1,24 +1,56 @@
 package org.gorzela.library.client.security;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import lombok.*;
+import org.gorzela.library.client.view.LoginFormView;
 import org.gorzela.library.common.Reader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Getter
 @Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Component
 public class CurrentReaderProvider {
 
-    private Reader loggedInReader;
+    @Autowired
+    private LoginFormView loginWindow;
 
-    public Reader checkCurrentReaderProvider(){
+    private Reader currentReader;
+    private int currentReaderSetupAccount;
 
-        return getLoggedInReader();
+
+
+    public Reader getCurrentReaderState () {
+
+        if(currentReader == null) {
+
+            System.out.println("Jestem w get");
+
+            Stage stage = new Stage();
+            Scene scene = (loginWindow.getView().getScene() == null ? new Scene(loginWindow.getView()) : loginWindow.getView().getScene());
+            stage.setScene(scene);
+            stage.setTitle("Logowanie");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+            System.out.println("po show");
+        }
+        System.out.println(currentReader);
+        return currentReader;
     }
+
+    //po wylogowaniu
+    public void unsetCurrentReader() {
+
+        currentReader = null;
+    }
+
+
+
 }
