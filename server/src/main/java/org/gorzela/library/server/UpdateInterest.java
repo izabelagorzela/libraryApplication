ff9b753e6c62c0ddb614f3cc8bcf898ad1ab0832
Iza <jza.q@wp.pl>
2018-06-06 19:51:51 +0200

    Work in progress

---
+++ server/src/main/java/org/gorzela/library/server/UpdateInterest.java
@@ -0,0 +1,63 @@
+package org.gorzela.library.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.gorzela.library.client.security.LibraryRestTemplateFactory;
+import org.gorzela.library.client.security.LibraryUriComponentsFactory;
+import org.gorzela.library.client.util.LibraryDate;
+import org.gorzela.library.common.Loan;
+import org.gorzela.library.common.Reader;
+import org.gorzela.library.server.repository.LoanRepository;
+import org.gorzela.library.server.repository.ReaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.transaction.Transactional;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class UpdateInterest {
+
+    private ArrayList<Loan> loansWithPayments;
+
+    @Autowired
+    LoanRepository loanRepository;
+
+    @Autowired
+    ReaderRepository readerRepository;
+
+    //every day at midnight
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void updateAllInterest() {
+
+        Date toDayDate = Date.from(LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        loansWithPayments = loanRepository.findByDateToBeforeAndReturnDateIsNull(toDayDate);
+
+        if (loansWithPayments.size() != 0) {
+
+            ArrayList<Reader> readerListWithPayment = new ArrayList<Reader>();
+            log.info("Interest update started...");
+            for (int i = 0; i < loansWithPayments.size(); i++) {
+
+                double newValue = loansWithPayments.get(i).getReader().getPayment() + 0.5;
+                loansWithPayments.get(i).getReader().setPayment(newValue);
+                readerListWithPayment.add(loansWithPayments.get(i).getReader());
+            }
+
+            readerRepository.save(readerListWithPayment);
+
+        }
+    }
+}
+
